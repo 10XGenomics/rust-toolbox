5e6587e01cee9d93227e40f267f96efbbbd79161
David Jaffe <david.jaffe@10xgenomics.com>
2020-04-30 05:39:31 -0700

    use https://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd not http:...

--- ansi_escape/src/ansi_to_html.rs
+++ ansi_escape/src/ansi_to_html.rs
@@ -306,7 +306,7 @@ fn html_head(
     format!(
         "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n\
               <!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \
-              \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n<!-- {} -->\n\
+              \"https://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n<!-- {} -->\n\
               <html xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n\
               <meta http-equiv=\"Content-Type\" content=\"application/xml+xhtml; \
               charset=UTF-8\"/>\n\
