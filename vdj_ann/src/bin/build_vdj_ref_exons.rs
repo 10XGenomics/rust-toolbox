a1422b8d09477c70b81f0c2f8db34d48993c0024
David Jaffe <david.jaffe@10xgenomics.com>
2020-10-09 13:51:08 -0700

    catch D genes

--- vdj_ann/src/bin/build_vdj_ref_exons.rs
+++ vdj_ann/src/bin/build_vdj_ref_exons.rs
@@ -77,6 +77,9 @@ fn parse_gtf_file(
         if !biotype.starts_with("IG_") {
             continue;
         }
+        if biotype.starts_with("IG_C") {
+            continue;
+        }
 
         // Exclude certain types.
 
@@ -122,7 +125,7 @@ fn parse_gtf_file(
 
         if !gene2.starts_with("IGHV") && !gene2.starts_with("IGKV") && !gene2.starts_with("IGLV")
             && !gene2.starts_with("IGHJ") && !gene2.starts_with("IGKJ") 
-            && !gene2.starts_with("IGLJ") {
+            && !gene2.starts_with("IGLJ") && !gene2.starts_with("IGHD") {
             continue;
         }
 
