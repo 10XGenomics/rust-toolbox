3a3db205892d83f140903466a7925e3edda7171c
David Jaffe <david.jaffe@10xgenomics.com>
2022-02-07 07:16:45 -0800

    add comment

--- vdj_ann_ref/src/bin/build_vdj_ref.rs
+++ vdj_ann_ref/src/bin/build_vdj_ref.rs
@@ -25,7 +25,7 @@
 //    build_vdj_ref MOUSE
 //    *** These must be run from the root of the repo! ***
 //    You don't need to do this unless you're changing this code.
-//    Running this for human can be very slow.
+//    Running this for human can take over an hour.
 //
 //    These files get ultimately moved to:
 //    /mnt/opt/refdata_cellranger/vdj/
