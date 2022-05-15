10XGenomics/rust-toolbox

commit 933ae108d60b238b11ef98a1c9575aaa9b52634a

Many API cleanups.
Fix a future-incompatible warning in some `io_util` macros due to trailing
semicolons.

Also fix those macros to work properly when expanded in contexts where
the types they're accessing aren't in scope, and allow them to work with
`impl AsRef<Path>` types rather than just `&String`.

Add trait implementations where appropriate, mostly `Default` but also a
few for `From` where it looked like it made sense given existing
conversion functions, in order to bring the APIs into conformance with
common naming conventions.

Fix a bunch of clippy lints around stuff that looks like
```rust
if f.is_err() {
    // thing
} else {
    let f = f.unwrap();
    // other thing
}
```
using `match` instead to avoid checking more than once whether the type
was an error.

Replace uses of `&Vec<T>` with `&[T]` where possible.  In some cases,
the extent to which this was possible is limited until some of the
upstream crates (from this repository) push a new version with their
APIs updated.

Replace uses of `&String` with either `&str` or `impl AsRef<Path>`
as appropriate for the use case.  Again, as with the `&Vec<T>` changes,
in some cases this means temporarily going through fallible steps
allocating String objects until the upstream crates publish versions
with updated APIs.

I could be wrong but I believe that `cargo clippy --fix` will be able
to fix the TODOs from the above two items automatically when the time
comes.

The above two changes _shouldn't_ affect existing users, at least in
most cases (`&Vec<T>` casts implicitly to `&[T]`, `&String` casts
implicitly to `&str`, and both `&String` and `&str` implement
`AsRef<Path>`).  At least, in theory.

Change the `segtype` field of `vdj_ann::refx::RefData` from a vector of
owned `String` to a `&'static str`, since everything added to that
vector is a constant value.  This should significantly reduce memory
consumption, as the vector is now a vector of pointers into constant

fasta_tools/src/lib.rs
@@ -9,58 +9,64 @@ use std::process::Command;
 use std::{
     fs::File,
     io::{prelude::*, BufReader},
+    path::Path,
 };
 use string_utils::TextUtils;
 
 // Read a fasta file or gzipped fasta file and convert to a Vec<Vec<u8>>, in which
 // outer vec entries alternate between header lines and base lines.
 
-pub fn read_fasta_to_vec_vec_u8(f: &str) -> Vec<Vec<u8>> {
+pub fn read_fasta_to_vec_vec_u8(f: impl AsRef<Path>) -> Vec<Vec<u8>> {
     let mut x = Vec::<Vec<u8>>::new();
-    if !f.ends_with(".gz") {
-        let fin = open_for_read![&f];
-        let mut last: String = String::new();
-        let mut first = true;
-        for line in fin.lines() {
-            let s = line.unwrap();
-            if first {
-                if !s.starts_with('>') {
-                    panic!("fasta format failure reading {}", f);
+    let f = f.as_ref();
+    match f.extension() {
+        Some(ex) if ex == "gz" => {
+            // TODO: pass `&Path` directly rather than `&str` once io_utils is updated.
+            let fin = open_for_read![f.to_str().unwrap()];
+            let mut last: String = String::new();
+            let mut first = true;
+            for line in fin.lines() {
+                let s = line.unwrap();
+                if first {
+                    if !s.starts_with('>') {
+                        panic!("fasta format failure reading {}", f.to_string_lossy());
+                    }
+                    first = false;
+                    x.push(s.get(1..).unwrap().as_bytes().to_vec());
+                } else if s.starts_with('>') {
+                    x.push(last.as_bytes().to_vec());
+                    last.clear();
+                    x.push(s.get(1..).unwrap().as_bytes().to_vec());
+                } else {
+                    last += &s;
                 }
-                first = false;
-                x.push(s.get(1..).unwrap().as_bytes().to_vec());
-            } else if s.starts_with('>') {
-                x.push(last.as_bytes().to_vec());
-                last.clear();
-                x.push(s.get(1..).unwrap().as_bytes().to_vec());
-            } else {
-                last += &s;
             }
+            x.push(last.as_bytes().to_vec());
         }
-        x.push(last.as_bytes().to_vec());
-    } else {
-        let gz = MultiGzDecoder::new(std::fs::File::open(&f).unwrap());
-        let fin = std::io::BufReader::new(gz);
-        let mut last: String = String::new();
-        let mut first = true;
-        for line in fin.lines() {
-            let s = line.unwrap();
-            if first {
-                if !s.starts_with('>') {
-                    panic!("fasta format failure");
+        _ => {
+            let gz = MultiGzDecoder::new(std::fs::File::open(&f).unwrap());
+            let fin = std::io::BufReader::new(gz);
+            let mut last: String = String::new();
+            let mut first = true;
+            for line in fin.lines() {
+                let s = line.unwrap();
+                if first {
+                    if !s.starts_with('>') {
+                        panic!("fasta format failure");
+                    }
+                    first = false;
+                    x.push(s.get(1..).unwrap().as_bytes().to_vec());
+                } else if s.starts_with('>') {
+                    x.push(last.as_bytes().to_vec());
+                    last.clear();
+                    x.push(s.get(1..).unwrap().as_bytes().to_vec());
+                } else {
+                    last += &s;
                 }
-                first = false;
-                x.push(s.get(1..).unwrap().as_bytes().to_vec());
-            } else if s.starts_with('>') {
-                x.push(last.as_bytes().to_vec());
-                last.clear();
-                x.push(s.get(1..).unwrap().as_bytes().to_vec());
-            } else {
-                last += &s;
             }
+            x.push(last.as_bytes().to_vec());
         }
-        x.push(last.as_bytes().to_vec());
-    }
+    };
     x
 }
 
@@ -69,58 +75,63 @@ pub fn read_fasta_to_vec_vec_u8(f: &str) -> Vec<Vec<u8>> {
 // ◼ Kill the code duplication below.
 
 pub fn read_fasta_into_vec_dna_string_plus_headers(
-    f: &String,
+    f: impl AsRef<Path>,
     dv: &mut Vec<DnaString>,
     headers: &mut Vec<String>,
 ) {
-    if !f.ends_with(".gz") {
-        let fin = open_for_read![&f];
-        let mut last: String = String::new();
-        let mut first = true;
-        for line in fin.lines() {
-            let s = line.unwrap();
-            if first {
-                if !s.starts_with('>') {
-                    panic!("fasta format failure reading {}", f);
+    let f = f.as_ref();
+    match f.extension() {
+        Some(ex) if ex == "gz" => {
+            // TODO: pass `&Path` directly rather than `&str` once io_utils is updated.
+            let fin = open_for_read![f.to_str().unwrap()];
+            let mut last: String = String::new();
+            let mut first = true;
+            for line in fin.lines() {
+                let s = line.unwrap();
+                if first {
+                    if !s.starts_with('>') {
+                        panic!("fasta format failure reading {}", f.to_string_lossy());
+                    }
+                    first = false;
+                    headers.push(s.get(1..).unwrap().to_string());
+                } else if s.starts_with('>') {
+                    dv.push(DnaString::from_dna_string(&last));
+                    last.clear();
+                    headers.push(s.get(1..).unwrap().to_string());
+                } else {
+                    last += &s;
                 }
-                first = false;
-                headers.push(s.get(1..).unwrap().to_string());
-            } else if s.starts_with('>') {
-                dv.push(DnaString::from_dna_string(&last));
-                last.clear();
-                headers.push(s.get(1..).unwrap().to_string());
-            } else {
-                last += &s;
             }
+            dv.push(DnaString::from_dna_string(&last));
         }
-        dv.push(DnaString::from_dna_string(&last));
-    } else {
-        let gz = MultiGzDecoder::new(std::fs::File::open(&f).unwrap());
-        let fin = std::io::BufReader::new(gz);
-        let mut last: String = String::new();
-        let mut first = true;
-        for line in fin.lines() {
-            let s = line.unwrap();
-            if first {
-                if !s.starts_with('>') {
-                    panic!("fasta format failure");
+        _ => {
+            let gz = MultiGzDecoder::new(std::fs::File::open(&f).unwrap());
+            let fin = std::io::BufReader::new(gz);
+            let mut last: String = String::new();
+            let mut first = true;
+            for line in fin.lines() {
+                let s = line.unwrap();
+                if first {
+                    if !s.starts_with('>') {
+                        panic!("fasta format failure");
+                    }
+                    first = false;
+                    headers.push(s.get(1..).unwrap().to_string());
+                } else if s.starts_with('>') {
+                    dv.push(DnaString::from_dna_string(&last));
+                    last.clear();
+                    headers.push(s.get(1..).unwrap().to_string());
+                } else {
+                    last += &s;
                 }
-                first = false;
-                headers.push(s.get(1..).unwrap().to_string());
-            } else if s.starts_with('>') {
-                dv.push(DnaString::from_dna_string(&last));
-                last.clear();
-                headers.push(s.get(1..).unwrap().to_string());
-            } else {
-                last += &s;
             }
+            dv.push(DnaString::from_dna_string(&last));
         }
-        dv.push(DnaString::from_dna_string(&last));
-    }
+    };
 }
 
 pub fn read_fasta_contents_into_vec_dna_string_plus_headers(
-    f: &String,
+    f: &str,
     dv: &mut Vec<DnaString>,
     headers: &mut Vec<String>,
 ) {
@@ -148,15 +159,17 @@ pub fn read_fasta_contents_into_vec_dna_string_plus_headers(
 
 // This APPENDS.
 
-pub fn read_fasta_headers(f: &String, headers: &mut Vec<String>) {
-    let fin = open_for_read![&f];
+pub fn read_fasta_headers(f: impl AsRef<Path>, headers: &mut Vec<String>) {
+    let f = f.as_ref();
+    // TODO: Don't convert to str.
+    let fin = open_for_read![f.to_str().unwrap()];
     let mut last: String = String::new();
     let mut first = true;
     for line in fin.lines() {
         let s = line.unwrap();
         if first {
             if !s.starts_with('>') {
-                panic!("fasta format failure reading {}", f);
+                panic!("fasta format failure reading {}", f.to_string_lossy());
             }
             first = false;
             headers.push(s.get(1..).unwrap().to_string());
@@ -173,7 +186,7 @@ pub fn read_fasta_headers(f: &String, headers: &mut Vec<String>) {
 // LOAD GENBANK ACCESSION
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 
-pub fn load_genbank_accession(accession: &String, bases: &mut DnaString) {
+pub fn load_genbank_accession(accession: &str, bases: &mut DnaString) {
     let link = format!(
         "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/\
          efetch.fcgi?db=nucleotide&amp;id={}&amp;rettype=fasta",
@@ -184,7 +197,7 @@ pub fn load_genbank_accession(accession: &String, bases: &mut DnaString) {
         .arg(format!("curl \"{}\"", link))
         .output()
         .expect("failed to execute curl command");
-    let fasta = String::from_utf8(o.stdout).unwrap();
+    let fasta = String::from_utf8(o.stdout).expect("Response was not valid utf-8.");
     let mut fasta = fasta.after("\n").to_string();
     // ◼ The following assert should not be necessary: the DnaString constructor
     // ◼ should gag if it gets nonsense input.

align_tools/src/lib.rs
@@ -198,7 +198,7 @@ pub fn affine_align(x: &DnaString, y: &DnaString) -> Alignment {
 // are used and would need to be tweaked if other operations are present.  You can set width to
 // the expected terminal width.
 
-pub fn vis_align(s1: &[u8], s2: &[u8], ops: &Vec<AlignmentOperation>, width: usize) -> String {
+pub fn vis_align(s1: &[u8], s2: &[u8], ops: &[AlignmentOperation], width: usize) -> String {
     let (mut pos1, mut pos2) = (0, 0);
     let (mut t1, mut t2) = (Vec::<u8>::new(), Vec::<u8>::new());
     let mut d = Vec::<u8>::new();

binary_vec_io/src/lib.rs
@@ -76,7 +76,7 @@ pub fn binary_read_to_ref<T>(f: &mut std::fs::File, p: &mut T, n: usize) -> Resu
 // in the first case, or to a vector, in the second case.
 
 #[allow(dead_code)]
-pub fn binary_write_vec<T>(f: &mut std::fs::File, x: &Vec<T>) -> Result<(), Error>
+pub fn binary_write_vec<T>(f: &mut std::fs::File, x: &[T]) -> Result<(), Error>
 where
     T: BinaryInputOutputSafe,
 {
@@ -112,7 +112,7 @@ where
     binary_read_to_ref::<T>(f, &mut x[len], n)
 }
 
-pub fn binary_write_vec_vec<T>(f: &mut std::fs::File, x: &Vec<Vec<T>>) -> Result<(), Error>
+pub fn binary_write_vec_vec<T>(f: &mut std::fs::File, x: &[Vec<T>]) -> Result<(), Error>
 where
     T: BinaryInputOutputSafe,
 {

dna/src/lib.rs
@@ -44,7 +44,7 @@ pub fn tm_nearest_neighbor(s: &str) -> f64 {
     tm_nearest_neighbor_full(s, 0.00000025, 0.05, &locked)
 }
 
-pub fn tm_nearest_neighbor_full(s: &str, s_mol: f64, na_mol: f64, locked: &Vec<bool>) -> f64 {
+pub fn tm_nearest_neighbor_full(s: &str, s_mol: f64, na_mol: f64, locked: &[bool]) -> f64 {
     // Allow for + symbols.
 
     if s.contains('+') {
@@ -52,11 +52,8 @@ pub fn tm_nearest_neighbor_full(s: &str, s_mol: f64, na_mol: f64, locked: &Vec<b
         assert!(!s.contains("++"));
         assert!(!s.ends_with('+'));
         let mut sx = String::new();
-        let mut lockedx = Vec::<bool>::new();
-        let mut schars = Vec::<char>::new();
-        for c in s.chars() {
-            schars.push(c);
-        }
+        let schars: Vec<char> = s.chars().collect();
+        let mut lockedx = Vec::<bool>::with_capacity(schars.len());
         let mut i = 0;
         while i < schars.len() {
             if schars[i] != '+' {
@@ -365,7 +362,7 @@ pub fn thermodynamic_sums_dna(
     dg_sum: &mut f64,
     include_symmetry_correction: bool,
     include_initiation_terms: bool,
-    locked: &Vec<bool>,
+    locked: &[bool],
 ) {
     // defaults for last: true, true, empty
 

exons/src/lib.rs
@@ -14,7 +14,7 @@ use std::{
 use string_utils::TextUtils;
 use vector_utils::unique_sort;
 
-pub fn fetch_exons(species: &String, exons: &mut Vec<(String, i32, i32, bool, String, i32)>) {
+pub fn fetch_exons(species: &str, exons: &mut Vec<(String, i32, i32, bool, String, i32)>) {
     assert!(species == "human" || species == "mouse");
 
     // Define gtf file location.  See notes in bin/build_vdj_ref.fs.

expr_tools/src/lib.rs
@@ -8,7 +8,7 @@
 use evalexpr::{ContextWithMutableFunctions, ContextWithMutableVariables, HashMapContext};
 use evalexpr::{Function, Value};
 use statrs::distribution::ContinuousCDF;
-use string_utils::*;
+use string_utils::TextUtils;
 use vector_utils::{bin_member, unique_sort};
 
 // ================================================================================================
@@ -124,7 +124,7 @@ macro_rules! evalexpr_fn3 {
 // to be called on arbitrary strings, but if the strings are not all f64, then the return value is
 // null.
 
-pub fn define_evalexpr_context(vars: &Vec<String>, vals: &Vec<String>) -> evalexpr::HashMapContext {
+pub fn define_evalexpr_context(vars: &[String], vals: &[String]) -> evalexpr::HashMapContext {
     assert_eq!(vars.len(), vals.len());
     let mut c = HashMapContext::new();
 

graph_simple/src/lib.rs
@@ -64,9 +64,9 @@ pub trait GraphSimple<T> {
     // get_predecessors1 and get_successors1: start from one vertex
     // =============================================================================
 
-    fn get_predecessors(&self, v: &Vec<i32>, x: &mut Vec<u32>);
+    fn get_predecessors(&self, v: &[i32], x: &mut Vec<u32>);
     fn get_predecessors1(&self, v: i32, x: &mut Vec<u32>);
-    fn get_successors(&self, v: &Vec<i32>, x: &mut Vec<u32>);
+    fn get_successors(&self, v: &[i32], x: &mut Vec<u32>);
     fn get_successors1(&self, v: i32, x: &mut Vec<u32>);
 
     // =============================================================================
@@ -171,7 +171,7 @@ where
         self.edge_obj(self.e_to(v, n) as u32)
     }
 
-    fn get_predecessors(&self, v: &Vec<i32>, x: &mut Vec<u32>) {
+    fn get_predecessors(&self, v: &[i32], x: &mut Vec<u32>) {
         let mut check: Vec<u32> = Vec::new();
         let mut tov: HashSet<u32> = HashSet::new();
         for j in 0..v.len() {
@@ -203,7 +203,7 @@ where
         self.get_predecessors(&vs, x);
     }
 
-    fn get_successors(&self, v: &Vec<i32>, x: &mut Vec<u32>) {
+    fn get_successors(&self, v: &[i32], x: &mut Vec<u32>) {
         let mut check: Vec<u32> = Vec::new();
         let mut fromv: HashSet<u32> = HashSet::new();
         for j in 0..v.len() {

hyperbase/src/lib.rs
@@ -60,12 +60,10 @@ pub fn debruijn_to_petgraph_hyperbasevector<K: Kmer>(
         // Update involution.
 
         let palindrome = seq == seq.rc();
-        if palindrome {
-            inv.push(edges.len() as u32);
-        } else {
+        if !palindrome {
             inv.push((edges.len() + 1) as u32);
-            inv.push(edges.len() as u32);
         }
+        inv.push(edges.len() as u32);
 
         // Save edge.
 
@@ -218,6 +216,12 @@ impl HyperBasevector {
     }
 }
 
+impl Default for HyperBasevector {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 // HYPER DEFINITION
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
@@ -247,7 +251,7 @@ impl Hyper {
     // Return the sequence associated to a path.
     // =============================================================================
 
-    pub fn cat(&self, p: &Vec<i32>) -> DnaString {
+    pub fn cat(&self, p: &[i32]) -> DnaString {
         let mut s = DnaString::new();
         if p.is_empty() {
             return s;
@@ -296,7 +300,7 @@ impl Hyper {
     // hide_seq: if true, and there is an annotation on an edge, don't print the sequence
     // ============================================================================================
 
-    pub fn print_with_annotations(&self, ann: &Vec<String>, require_ann: bool, hide_seq: bool) {
+    pub fn print_with_annotations(&self, ann: &[String], require_ann: bool, hide_seq: bool) {
         let mut comp = Vec::<Vec<u32>>::new();
         self.h.g.components_e(&mut comp);
         let mut n = 0;
@@ -359,7 +363,7 @@ impl Hyper {
     // Create a new Hyper from data.
     // =============================================================================
 
-    pub fn build_from_reads(&mut self, k: i32, reads: &Vec<DnaString>) {
+    pub fn build_from_reads(&mut self, k: i32, reads: &[DnaString]) {
         // Only works if k = 20.
 
         assert_eq!(k, 20);
@@ -503,10 +507,10 @@ impl Hyper {
     // Normally you would want to call kill_edges instead.
     // =============================================================================
 
-    pub fn kill_edges_raw(&mut self, dels: &Vec<u32>) {
+    pub fn kill_edges_raw(&mut self, dels: &[u32]) {
         // Symmetrize and unique sort dels.
 
-        let mut dels2 = dels.clone();
+        let mut dels2 = dels.to_vec();
         for e in dels {
             dels2.push(self.inv[*e as usize]);
         }
@@ -547,7 +551,7 @@ impl Hyper {
     // documentation in those files that describes some of the logic.
     // =============================================================================
 
-    pub fn kill_edges(&mut self, dels: &Vec<u32>) {
+    pub fn kill_edges(&mut self, dels: &[u32]) {
         // Kill the edges.
 
         self.kill_edges_raw(dels);
@@ -940,5 +944,11 @@ impl Hyper {
             }
         }
         z
+    }
+}
+
+impl Default for Hyper {
+    fn default() -> Self {
+        Self::new()
     }
 }

commit 2c5cce28f5b4ae0a08ff53aead14bfd08bad39ab

fix bug introduced in 933ae108
The interpretation of .gz was reversed by that commit.

fasta_tools/src/lib.rs
@@ -76,7 +76,7 @@ pub fn read_fasta_into_vec_dna_string_plus_headers(
 ) {
     let f = f.as_ref();
     match f.extension() {
-        Some(ex) if ex == "gz" => {
+        Some(ex) if ex != "gz" => {
             let fin = open_for_read![f];
             let mut last: String = String::new();
             let mut first = true;